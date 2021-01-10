606940f275f17de3ae54b8c92f97ba42ef2b7118
Muriel Jakazi <murielnjakazi@gmail.com>
2021-01-10 17:09:40 +0100

    step 2: added code to be tested

--- tdd1/capitalizeFirstLetters.js
+++ tdd1/capitalizeFirstLetters.js
@@ -1,5 +1,15 @@
 const assert = require('assert');
 
+function capitalizeFirtsLetters(input) {
+    const strings = input.split(" ");
+    const result = strings.map((string, index) => {
+        return string.length > 0 
+        ? string[0].toUpperCase() + string.slice(1)
+        : ""
+    });
+    return result.join(' ');
+};
+
 assert.strictEqual(typeof capitalizeFirtsLetters, 'function');
 assert.strictEqual(capitalizeFirtsLetters.length, 1);
 assert.strictEqual(capitalizeFirtsLetters('hello world'), 'Hello World');
